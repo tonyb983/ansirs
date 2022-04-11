177d0ca77532f7e26178ec77b3e231db8a7c64d6
Tony B <tonyb983@gmail.com>
2022-04-11 12:03:49 -0400

    quick fix in styled/str.rs

--- src/styled/str.rs
+++ src/styled/str.rs
@@ -4,7 +4,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub struct PrettyString(todo!());
+use crate::Ansi;
+
+pub struct PrettyString(/* todo!() */);
 
 impl PrettyString {
     pub fn new(text: &str, format: Ansi) -> Self {
