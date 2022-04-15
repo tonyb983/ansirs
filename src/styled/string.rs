644596e7ba1e7139fefe20bd0acfce9f48bc54df
Eric Githinji <ericgithinji@gmail.com>
2022-04-15 17:14:59 +0300

    second implementation of prettystring

--- src/styled/string.rs
+++ src/styled/string.rs
@@ -9,30 +9,31 @@
 
 use crate::Ansi;
 
+const SUFFIX_LENGTH:usize = 7;
+
 pub struct PrettyString {
-    text: String,
-    format: Ansi,
     formatted_text: String,
+    length_original: usize,
 }
 
 impl PrettyString {
     /// Create a new [`PrettyString`] from the given text and style.
     pub fn new(text: &str, format: Ansi) -> Self {
         PrettyString {
-            text: text.to_string(),
-            format,
             formatted_text: format.paint_text(text),
+            length_original: text.len(),
         }
     }
 
     /// Get the raw (unstyled) text contained in this [`PrettyString`].
     pub fn raw(&self) -> &str {
-        &self.text
+        let (_, o) = self.get_style_and_original();
+        o
     }
 
     /// Get the current style of this [`PrettyString`].
-    pub fn style(&self) -> &Ansi {
-        &self.format
+    pub fn style(&self) -> Option<Ansi> {
+        Ansi::parse_ansi_text(&self.formatted_text)
     }
 
     /// Get the formatted text contained in this [`PrettyString`].
@@ -44,9 +45,18 @@ impl PrettyString {
     ///
     /// `F` will receive the current style and should return the new or modified style.
     pub fn modify_style<F: FnOnce(Option<Ansi>) -> Option<Ansi>>(&mut self, f: F) {
-        if let Some(f) = f(Some(self.format)) {
-            self.format = f;
+        if let Some(f) = f(self.style()) {
+            let (original_t, _) = self.get_style_and_original();
+            let p = PrettyString::new(original_t, f);
+            self.formatted_text = p.formatted_text;
         }
+    }
+
+    fn get_style_and_original(&self) -> (&str, &str) {
+        let formatting_length = self.formatted_text.len() - SUFFIX_LENGTH - self.length_original;
+        let style = &self.formatted_text[0..formatting_length];
+        let original = &self.formatted_text[formatting_length..self.formatted_text.len() - SUFFIX_LENGTH];
+        (style, original)
     }
 }
 
